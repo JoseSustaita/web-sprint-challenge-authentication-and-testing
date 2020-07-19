3a5f22305d7b9446d0fa6bd7c1e2aa5595eace8a
Jose Sustaita <josesustaita70@gmail.com>
2020-07-19 19:50:30 -0400

    Built out auth and middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,22 @@
+const jwt = require("jsonwebtoken");
+
 /* 
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  try {
+    const token = req.headers.token.split("")[1];
+    if (!token) {
+      return res.status(401).json({ message: "Missing token!" });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+      req.user = user;
+      next();
+    });
+  } catch (err) {
+    res.status(401).json({ you: "shall not pass!" });
+  }
 };
